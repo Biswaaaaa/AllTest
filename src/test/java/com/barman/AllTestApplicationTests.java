6f93df8a386901b568cbf8e90d235eb09aa03d69
Biswaaaaa <ibbarman@gmail.com>
2020-07-07 22:15:15 +0530

    Initial commit

---
+++ src/test/java/com/barman/AllTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
